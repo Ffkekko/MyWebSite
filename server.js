a20d3e8ee46eea3a588054b04cb7e84e3fcf2b59
Ffkekko <fnc.franzese1@gmail.com>
2020-10-02 21:39:46 +0200

    fixed deployment problem adding frontend before backend

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const {createServer} = require('http');
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+
+
+const normalizePort = val => { 
+    const PORT = parseInt(val, 10);
+  
+    if (isNaN(PORT)) {
+      return val;
+    }
+    if (PORT >= 0) {
+      return PORT;
+    }
+    return false;
+};/* the  normalizePort  function returns a valid port, whether it is provided as a number or a string */
+
 
 
 require('dotenv').config(); 
@@ -11,6 +27,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
+
+
+const PORT = normalizePort(process.env.PORT||3001); /* we set this in react frontend package.json "proxy":"http://localhost:3001", and that's where we are getting the data*/
+app.set('PORT', PORT); /* We normalised the port with the constant port above. see 2.Step  */
+
+if (process.env.NODE_ENV === 'production') { /* remember to update this in heroku config vars */
+    app.use(express.static('frontend/build')); /* you can also try express.static(path.resolve(__dirname, 'build')) */
+}
+
+app.use(express.static(path.resolve(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend/build/', 'index.html'));
+});
 
 
 //Contact form backend
@@ -68,12 +97,9 @@ app.post('/api/form', (req,res)=> {
 
 //End contact form backend
 
-const PORT = process.env.PORT||3001; /* we set this in react frontend package.json "proxy":"http://localhost:3001", and that's where we are getting the data*/
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-}
+const server = createServer(app); /* here, the listener we had used in the step 1 was a hard coded function with req and res. but here is where now we are gonna import our app. so first we import it in const app and then we pass it as an argument. so basically node creates the server by using our express app */;
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`server starting at port ${PORT}`)
 })
