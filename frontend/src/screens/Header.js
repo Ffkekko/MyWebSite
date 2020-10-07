e819498f9cd3b8f6e1a31a460e500caa508710ac
Ffkekko <fnc.franzese1@gmail.com>
2020-10-08 00:43:19 +0200

    fixing problem with menu for ciccio

--- frontend/src/screens/Header.js
+++ frontend/src/screens/Header.js
@@ -67,8 +67,9 @@ class Header extends React.Component {
                         <li><a href="#about" data-after="About">About</a></li>
                         <li><a href="#contact" data-after="Contact">Contact</a></li>
                     </ul>
-                    </div>
                     <div className={`${headerActive}`}></div>
+                    </div>
+                    
                 </div>
               
                 </div>
