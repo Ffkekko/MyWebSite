c91d0819ab61707749101bcb8d9bb05863822b10
Ffkekko <fnc.franzese1@gmail.com>
2020-12-10 19:21:28 +0200

    added redirect https in server.js

--- server.js
+++ server.js
@@ -25,6 +25,19 @@ require('dotenv').config();
 
 const app = express();
 
+const redirectionFilter = function (req, res, next) {
+    const theDate = new Date();
+    const receivedUrl = `${req.protocol}:\/\/${req.hostname}:${port}${req.url}`;
+  
+    if (req.get('X-Forwarded-Proto') === 'http') {
+      const redirectTo = `https:\/\/${req.hostname}${req.url}`;
+      console.log(`${theDate} Redirecting ${receivedUrl} --> ${redirectTo}`);
+      res.redirect(301, redirectTo);
+    } else {
+      next();
+    }
+  };
+
 app.use(compression()); /* this is good for speed time, suggested by SEO  */
 
 
