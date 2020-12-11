21b029cc672df59b6e78e73509ffdf3a4900b4e7
Ffkekko <fnc.franzese1@gmail.com>
2020-12-11 15:45:56 +0200

    installed HttpsRedirect

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HttpsApp from './HttpsApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HttpsApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
